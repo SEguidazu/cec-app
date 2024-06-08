947bf194de2d76d920d05ca16cab290bbf2e9d99
Santiago Eguidazu <eguidazu.san@gmail.com>
2024-06-08 01:41:22 -0300

    actualizacion del login y layout a la version 2

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -1,17 +1,9 @@
 import { Outlet } from "react-router-dom";
 
-import EscudoCECLMSM from "@/assets/images/cec-liceo-militar.png";
-
 const Layout = () => {
   return (
-    <main className="App bg-[url('@/assets/images/buffet-blur-background.png')] bg-cover bg-center bg-cec_primaryDarker relative">
+    <main className="App h-screen bg-cover bg-center bg-cec_primaryDarker relative">
       <Outlet />
-
-      <img
-        src={EscudoCECLMSM}
-        alt=""
-        className="max-w-52	mx-auto absolute inset-x-0 bottom-6"
-      />
     </main>
   );
 };
