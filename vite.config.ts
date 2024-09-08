17a8ea1a026cc2429e3932417ff8f6dcab23f9e7
Santiago Eguidazu <eguidazu.san@gmail.com>
2024-09-08 02:26:46 -0300

    arregla la configuracion de cors para que utilice la variable de entorno

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 
+const API_URL = process.env.VITE_API_URL ?? "http://localhost:5000";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -15,34 +17,38 @@ export default defineConfig({
         name: "Socios CEC",
         short_name: "Socios CEC",
         description: "Web para acceso de socios del club CEC Liceo Militar",
-        theme_color: "#0F1F30",
-        background_color: "#0F1F30",
+        theme_color: "#0A4C7D",
+        background_color: "#0A4C7D",
         icons: [
           {
-            "src": "pwa-64x64.png",
-            "sizes": "64x64",
-            "type": "image/png"
+            src: "favicon-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
           },
           {
-            "src": "pwa-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png"
+            src: "favicon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
           },
           {
-            "src": "pwa-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png"
+            src: "favicon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
           },
-          {
-            "src": "maskable-icon-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "maskable"
-          }
         ],
       },
     }),
   ],
+  server: {
+    cors: {
+      origin: API_URL,
+      methods: ["GET", "POST"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      preflightContinue: true,
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
