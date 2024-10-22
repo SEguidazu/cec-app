7a5641d845ac09f1a07d41b99e83655c4fed4bc4
Santiago Eguidazu <eguidazu.san@gmail.com>
2024-10-22 15:52:35 -0300

    cambia mail de secretaria por el instagram del club en el footer

--- src/components/custom/footer.tsx
+++ src/components/custom/footer.tsx
@@ -1,4 +1,5 @@
 import { Phone, Mail } from "lucide-react";
+import Instagram from "@/assets/icons/Instagram.svg";
 import LiceoMilitarCEC from "@/assets/images/cec-liceo-militar.png";
 import { cn } from "@/lib/utils";
 
@@ -7,33 +8,45 @@ interface Props {
 }
 
 function Footer({ isContained = false }: Props) {
-
   return (
     <footer className="flex flex-col justify-center items-center gap-y-4 mt-6">
-      <div className={cn(
-        isContained ? 'max-w-72 px-3 p-5' : 'py-2',
-        'w-full mb-2 rounded-lg bg-white')}>
+      <div
+        className={cn(
+          isContained ? "max-w-72 px-3 p-5" : "py-2",
+          "w-full mb-2 rounded-lg bg-white"
+        )}
+      >
         <h2 className="font-body font-bold text-md text-black mb-2">
           Medios de Contacto
         </h2>
 
         <ul>
           <li>
-            <a className="inline-flex flex-row items-start justify-center text-sm gap-x-2 leading-6" href="tel:39820217">
+            <a
+              className="inline-flex flex-row items-start justify-center text-sm gap-x-2 leading-6"
+              href="tel:39820217"
+            >
               <Phone color="#0A4C7D" />
               3982-0217
             </a>
           </li>
           <li>
-            <a className="inline-flex flex-row items-start justify-center text-sm gap-x-2 leading-6" href="mailto:consultassocios@yahoo.com.ar">
+            <a
+              className="inline-flex flex-row items-start justify-center text-sm gap-x-2 leading-6"
+              href="mailto:consultassocios@yahoo.com.ar"
+            >
               <Mail color="#0A4C7D" />
               consultassocios@yahoo.com.ar
             </a>
           </li>
           <li>
-            <a className="inline-flex flex-row items-start justify-center text-sm gap-x-2 leading-6" href="mailto:secretariacecliceomilitargsm@yahoo.com.ar">
-              <Mail color="#0A4C7D" />
-              secretariacecliceomilitargsm<br />@yahoo.com.ar
+            <a
+              className="inline-flex flex-row items-start justify-center text-sm gap-x-2 leading-6"
+              href="https://www.instagram.com/cecliceomilitar"
+              target="_blank"
+            >
+              <img src={Instagram} alt="" className="w-6 h-6" />
+              @cecliceomilitar
             </a>
           </li>
         </ul>
@@ -43,8 +56,7 @@ function Footer({ isContained = false }: Props) {
         <img src={LiceoMilitarCEC} alt="" className="max-w-40	mx-auto mb-4" />
       )}
     </footer>
-  )
+  );
 }
 
-export default Footer
-
+export default Footer;
