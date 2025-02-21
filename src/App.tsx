2364f7014dd71f4c82019027f8338b3bf393cd1d
Santiago Eguidazu <eguidazu.san@gmail.com>
2025-02-20 21:18:03 -0300

    simplifica a un archivo de App las rutas

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import Layout from "@/pages/layout";
+import Login from "@/pages/login";
+import Dashboard from "@/pages/dashboard";
+import MemberDetails from "@/pages/memberDetails";
+
+import UserAuthentication from "@/routes/guard/UserAuthentication";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<Layout />}>
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+
+          <Route path="/login" element={<Login />} />
+
+          <Route element={<UserAuthentication />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/dashboard/:memberId" element={<MemberDetails />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
